81335805b1896b25dabd890cd48cd2fffc120704
Sean Doyle <sean.p.doyle24@gmail.com>
2021-08-21 19:33:46 -0400

    [BUGFIX]: Redirect `_top` from within `turbo-frame`

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,9 +2,21 @@
 
 // import "@rails/actioncable"
 // import "@rails/activestorage"
-import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
 
 import "@hotwired/stimulus-importmap-autoloader"
 
 import "trix"
 import "@rails/actiontext"
+
+addEventListener("turbo:submit-start", (event) => {
+  const form = event.target
+
+  if (form instanceof HTMLFormElement) {
+    form.addEventListener("turbo:submit-end", ({ target, detail: { fetchResponse } }) => {
+      if (fetchResponse.redirected && fetchResponse.header("Turbo-Frame") == "_top") {
+        Turbo.visit(fetchResponse.location)
+      }
+    }, { once: true })
+  }
+})
