2c684285eebe470dde3e1c2cf990ee44ca5c859c
Sean Doyle <sean.p.doyle24@gmail.com>
2021-08-21 19:36:29 -0400

    [GENERATED]: Scaffold Message model
    ```sh
    rails generate scaffold Message author:string:index
    rails db:migrate
    ```

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_002321) do
+ActiveRecord::Schema.define(version: 2021_08_30_020528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,20 @@ ActiveRecord::Schema.define(version: 2021_08_30_002321) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "author"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author"], name: "index_messages_on_author"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_users_on_name"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
