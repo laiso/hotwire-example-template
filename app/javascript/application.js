44918b354ff1cb6d7cd26d3827a0e0befb9f10cf
Sean Doyle <sean.p.doyle24@gmail.com>
2021-08-21 19:33:46 -0400

    [BUGFIX]: Redirect `_top` from within `turbo-frame`

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "tailwind.config"
-import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
+
+addEventListener("turbo:submit-start", (event) => {
+  const form = event.target
+
+  if (form instanceof HTMLFormElement) {
+    form.addEventListener("turbo:submit-end", ({ target, detail: { fetchResponse } }) => {
+      if (fetchResponse.redirected && fetchResponse.header("Turbo-Frame") == "_top") {
+        Turbo.visit(fetchResponse.location)
+      }
+    }, { once: true })
+  }
+})
