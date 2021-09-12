3d4b4804add8eb6fdbc92e6d3e8297167bf988f1
Sean Doyle <sean.p.doyle24@gmail.com>
2021-09-12 18:52:36 -0400

    [GENERATED]: PersonalReference
    ```sh
    rails generate model PersonalReference \
      name:text \
      email_address:text \
      applicant:references
    rails db:migrate
    ```

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_10_231011) do
+ActiveRecord::Schema.define(version: 2021_12_11_014259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,16 @@ ActiveRecord::Schema.define(version: 2021_12_10_231011) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "personal_references", force: :cascade do |t|
+    t.text "name"
+    t.text "email_address"
+    t.bigint "applicant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["applicant_id"], name: "index_personal_references_on_applicant_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "personal_references", "applicants"
 end
