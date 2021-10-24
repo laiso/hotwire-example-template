a2a48033b9265705869c866eec305dc2df5a5234
Sean Doyle <sean.p.doyle24@gmail.com>
2021-10-24 13:42:06 -0400

    [BACKPORT]: Override how Turbo Frames render
    [hotwired/turbo#146]: https://github.com/hotwired/turbo/pull/146

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,36 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
+
+const getTurboFrame = (element) => {
+  const id = element.getAttribute("data-turbo-frame")
+  const target = id ?
+    document.querySelector(`turbo-frame#${id}:not([disabled]):not([target="_top"])`) :
+    null
+
+  return target || element.closest(`turbo-frame:not([disabled]):not([target="_top"])`)
+}
+
+addEventListener("click", ({ target }) => {
+  if (target.matches("a")) {
+    const frame = getTurboFrame(target)
+    const rendering = target.getAttribute("data-turbo-rendering")
+
+    if (frame && rendering) {
+      const documentFragment = new DocumentFragment()
+
+      frame.addEventListener("turbo:before-fetch-response", () => {
+        documentFragment.replaceChildren(...frame.children)
+      }, { once: true })
+
+      frame.addEventListener("turbo:frame-render", () => {
+        if (rendering == "append") {
+          documentFragment.append(...frame.children)
+        } else if (rendering == "prepend") {
+          documentFragment.prepend(...frame.children)
+        }
+        frame.replaceChildren(documentFragment)
+      }, { once: true })
+    }
+  }
+}, { passive: true })
