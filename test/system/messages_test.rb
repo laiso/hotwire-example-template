5ae6ef2e45787bf517189340e8ea339e5bcaa69f
Sean Doyle <sean.p.doyle24@gmail.com>
2021-10-24 12:55:45 -0400

    Link to next page

--- test/system/messages_test.rb
+++ test/system/messages_test.rb
@@ -16,6 +16,22 @@ class MessagesTest < ApplicationSystemTestCase
     end
   end
 
+  test "links to the next page-worth of Message records" do
+    using_page_size 10 do |page_size|
+      messages = Message.most_recent_first.offset(page_size).limit(page_size)
+
+      visit messages_path
+      click_on("Next page") { _1["rel"] == "next" }
+
+      assert_css "article", count: page_size
+      messages.each_with_index do |message, index|
+        within "article:nth-of-type(#{index + 1})" do
+          assert_text message.content.to_plain_text
+        end
+      end
+    end
+  end
+
   def using_page_size(size = Pagy::DEFAULT[:items], &block)
     original_size, Pagy::DEFAULT[:items] = Pagy::DEFAULT[:items], size
 
