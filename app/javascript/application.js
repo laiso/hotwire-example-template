29acb12e9fb294ee1c787644526eb6ab37d423d0
Sean Doyle <sean.p.doyle24@gmail.com>
2021-11-20 09:32:28 -0500

    use `turbo:frame-missing`

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,18 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "tailwind.config"
-import { Turbo } from "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
-
-addEventListener("turbo:submit-start", (event) => {
-  const form = event.target
-
-  if (form instanceof HTMLFormElement) {
-    form.addEventListener("turbo:submit-end", ({ target, detail: { fetchResponse } }) => {
-      if (fetchResponse.redirected && fetchResponse.header("Turbo-Frame") == "_top") {
-        Turbo.visit(fetchResponse.location)
-      }
-    }, { once: true })
-  }
-})
