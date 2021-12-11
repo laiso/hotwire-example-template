478524afe64d9213a9946f2c4464f08de112c8a3
Sean Doyle <sean.p.doyle24@gmail.com>
2021-12-11 16:49:40 -0500

    Preserve fields across visits with targets and callbacks

--- app/javascript/controllers/session_resume_controller.js
+++ app/javascript/controllers/session_resume_controller.js
@@ -2,17 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 import { persistResumableFields, restoreResumableFields, setForm } from "https://cdn.skypack.dev/@github/session-resume"
 
 export default class extends Controller {
-  static get values() { return { selector: String } }
+  static get targets() { return [ "field" ] }
 
   setForm(event) {
     setForm(event)
   }
 
   cache() {
-    persistResumableFields(getPageID(), { selector: this.selectorValue })
+    const selector = `[data-${this.identifier}-target="field"]`
+
+    persistResumableFields(getPageID(), { selector })
   }
 
-  read() {
+  fieldTargetConnected() {
     restoreResumableFields(getPageID())
   }
 }
